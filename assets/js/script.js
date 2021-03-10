5268d29689af7b26600479e03ce0dcc17e4d56c3
[Andrew Core] <[adcore89@gmail.com]>
2021-03-09 21:05:19 -0500

    alert and enter number function created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 // Assignment code here
 
 
+document.querySelector("#generate").addEventListener("click", promptMe);
+
+// password length function 
+function promptMe () {
+  var userPasswordLength = prompt ("welcome to the passowrd generator, please select password length between 8 and 138 to continue");
+  if (userPasswordLength < 8) {
+    alert ("please select a number greater than 8");
+  }  
+  if(userPasswordLength > 138) {
+    alert ("please select a number less than 138")
+  }
+  if (userPasswordLength === isNaN()) {
+    alert("please select only a number")
+  }
+  if (isNaN(userPasswordLength)){
+  alert("Please enter a number between 8 and 138")
+  }
+};
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +38,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
