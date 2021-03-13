9d96e0a9df84b6ca0349b4cf5d58fe13eccba1b7
[Andrew Core] <[adcore89@gmail.com]>
2021-03-13 15:10:09 -0500

    variables set, and promp criteria coded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,117 @@
-// Assignment code here
+// variables 
+
+var number = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()_+";
+var upperCaseArray = upperCase.split("");
+var lowerCaseArray = lowerCase.split("");
+var numberArray = number.split("");
+var specialCharArray = specialChar.split(""); 
+
+// User prompt to begin password generation process 
+
+ alert("Welcome to the Password Generator, to get started please select 'generate password'"); 
+
+ document.querySelector("#generate").addEventListener("click", genPwd);
+
+//generate password function 
+
+function genPwd (){
+  var selectChar = [];
+  var pwdFinal = "";
+
+//set password length 
+
+var pwdLength = prompt("Please select a password length between 8 and 128 characters");
+
+//set password length , need to refactor to make recursive untill right answer is selected 
+
+if (pwdLength < 8 || pwdLength > 138 || isNaN(pwdLength)) {
+  prompt("Please enter a number between 8 and 138")
+if (pwdLength < 8 || pwdLength > 138 || isNaN(pwdLength)) 
+  prompt("Please enter a number between 8 and 138")
+if (pwdLength < 8 || pwdLength > 138 || isNaN(pwdLength)) 
+alert("You have made too many incorrect selections, please start again")  
+}
+
+// select other variables and push into array that final password will be generated from. 
+
+else{ 
+
+  if(confirm("Please click Ok to include special characters in your password")){
+    Array.prototype.push.apply(selectChar, specialCharArray);
+}
+
+ if(confirm(" Please click Ok to include lowercase letters in your password")){
+   Array.prototype.push.apply(selectChar, lowerCaseArray);
+}
+
+ if(confirm(" Please click OK to include Uppercase letters in your password")){
+  Array.prototype.push.apply(selectChar, upperCaseArray);
+
+}
+
+if(confirm("Please click OK to include numbers in your password")){
+ Array.prototype.pish.apply(selectChar, numberArray); 
+}
+
+if(confirm("Please click OK to include special characters in your password ")){
+  Array.prototype.push.apply(selectChar, specialCharArray);
+}
+
+if(selectChar.length===0){
+  alert("you did not select any password critera. Please note in order to generate your password you must select at least one of the options above. Please start over, thank you. ")
+}
+
+}
+// for loop 
 
 
-document.querySelector("#generate").addEventListener("click", promptMe);
 
-// password length function 
-function promptMe () {
-  var userPasswordLength = prompt ("welcome to the passowrd generator, please select password length between 8 and 138 to continue");
-
-  if (userPasswordLength < 8 || userPasswordLength > 138 || isNaN(userPasswordLength)) {
-  alert("Please enter a number between 8 and 138")
-  }
-  else prompt ("Thank you")
 
 };
+
+
+
+// for loop to confirm info and create password 
+
+// display the result 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 //generateBtn.addEventListener("click", writePassword);
+
+
+//document.querySelector("#generate").addEventListener("click", pwdGenerate);
